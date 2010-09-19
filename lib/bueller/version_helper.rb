7d6060bbce902ce246be780ac54c85286ba4ff24
Derek Kastner <dkastner@gmail.com>
2010-09-19 16:53:36 -0400

    Converted VersionHelper

--- lib/bueller/version_helper.rb
+++ lib/bueller/version_helper.rb
@@ -2,94 +2,34 @@ require 'yaml'
 
 class Bueller
   class VersionHelper
-    attr_accessor :base_dir
+    attr_accessor :gemspec
     attr_reader :major, :minor, :patch, :build
 
-    module YamlExtension
-      def write
-        File.open(yaml_path, 'w+') do |f|
-          YAML.dump(self.to_hash, f)
-        end
-      end
+    def initialize(gemspec)
+      self.gemspec = gemspec
+      parse_gemspec
+    end
 
-      def to_hash
-        {
-          :major => major,
-          :minor => minor,
-          :patch => patch,
-          :build => build
-        }
-      end
-
-      def refresh
-        parse_yaml
-      end
-
-      def parse_yaml
-        yaml = read_yaml
-        @major = (yaml['major'] || yaml[:major]).to_i
-        @minor = (yaml['minor'] || yaml[:minor]).to_i
-        @patch = (yaml['patch'] || yaml[:patch]).to_i
-        @build = (yaml['build'] || yaml[:build])
-      end
-
-      def read_yaml
-        if File.exists?(yaml_path)
-          YAML.load_file(yaml_path)
-        else
-          raise VersionYmlError, "#{yaml_path} does not exist!"
-        end
-      end
-
-      def path
-        yaml_path
+    def write
+      File.open(gemspec_path, 'w') do |file|
+        file.write to_s
       end
     end
 
-    module PlaintextExtension
-      def write
-        File.open(plaintext_path, 'w') do |file|
-          file.write to_s
-        end
-      end
-
-      def parse_plaintext
-        plaintext = read_plaintext.chomp
-        # http://rubular.com/regexes/10467 -> 3.5.4.a1
-        # http://rubular.com/regexes/10468 -> 3.5.4
-        if plaintext =~ /^(\d+)\.(\d+)\.(\d+)(?:\.(.*?))?$/
-          @major = $1.to_i
-          @minor = $2.to_i
-          @patch = $3.to_i
-          @build = $4
-        end
-      end
-
-      def read_plaintext
-        File.read(plaintext_path)
-      end
-
-      def refresh
-        parse_plaintext
-      end
-
-      def path
-        plaintext_path
+    def parse_gemspec
+      self.gemspec ||= eval(File.read(gemspec_path))
+      # http://rubular.com/regexes/10467 -> 3.5.4.a1
+      # http://rubular.com/regexes/10468 -> 3.5.4
+      if gemspec.version.to_s =~ /^(\d+)\.(\d+)\.(\d+)(?:\.(.*?))?$/
+        @major = $1.to_i
+        @minor = $2.to_i
+        @patch = $3.to_i
+        @build = $4
       end
     end
 
-    def initialize(base_dir)
-      self.base_dir = base_dir
-
-      if File.exists?(yaml_path)
-        extend YamlExtension
-        parse_yaml
-      else
-        extend PlaintextExtension
-        if File.exists?(plaintext_path)
-          parse_plaintext
-        end
-      end
+    def refresh
+      parse_gemspec
     end
 
     def bump_major
@@ -119,18 +59,6 @@ class Bueller
 
     def to_s
       [major, minor, patch, build].compact.join('.')
-    end
-
-    def yaml_path
-      denormalized_path = File.join(@base_dir, 'VERSION.yml')
-      absolute_path = File.expand_path(denormalized_path)
-      absolute_path.gsub(Dir.getwd + File::SEPARATOR, '')
-    end
-
-    def plaintext_path
-      denormalized_path = File.join(@base_dir, 'VERSION')
-      absolute_path = File.expand_path(denormalized_path)
-      absolute_path.gsub(Dir.getwd + File::SEPARATOR, '')
     end
   end
 end
---
+++ spec/bueller/version_helper_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Bueller::VersionHelper do
+
+  VERSION_TMP_DIR = File.dirname(__FILE__) + '/version_tmp'
+
+  let(:helper) { Bueller::VersionHelper.new gemspec }
+
+  describe "full version" do
+    let(:gemspec) do
+      Gem::Specification.new do |s|
+        s.version = '3.5.4'
+      end
+    end 
+
+    it 'should have version 3.5.4' do
+      helper.should have_version(3, 5, 4)
+    end
+
+    it "should bump major version" do
+      helper.bump_major
+      helper.should have_version(4, 0, 0)
+    end
+
+    it "should bump the minor version" do
+      helper.bump_minor
+      helper.should have_version(3, 6, 0)
+    end
+
+    it "should bump the patch version" do
+      helper.bump_patch
+      helper.should have_version(3, 5, 5)
+    end
+  end
+
+  describe "prerelease version" do
+    let(:gemspec) do
+      Gem::Specification.new do |s|
+        s.version = '3.5.4.a1'
+      end
+    end 
+
+    it 'should be version 3.5.4.a1' do
+      helper.should have_build_version(3, 5, 4, 'a1')
+    end
+
+    it "should bump major version" do
+      helper.bump_major
+      helper.should have_build_version(4, 0, 0, nil)
+    end
+
+    it "should bump the minor version" do
+      helper.bump_minor
+      helper.should have_build_version(3, 6, 0, nil)
+    end
+
+    it "should bump the patch version" do
+      helper.bump_patch
+      helper.should have_build_version(3, 5, 5, nil)
+    end
+  end
+end
