33172a4c12a6402c971b15b9c22c2138088303d6
Derek Kastner <dkastner@gmail.com>
2010-09-19 09:01:42 -0400

    Bueller spec

--- lib/bueller.rb
+++ lib/bueller.rb
@@ -26,15 +26,13 @@ class Bueller
   attr_accessor :base_dir, :output, :repo, :commit
 
   def initialize(gemspec, base_dir = '.')
-    raise(GemspecError, "Can't create a Bueller with a nil gemspec") if gemspec.nil?
-
     @gemspec = gemspec
     @gemspec.extend(Specification)
     @gemspec.set_bueller_defaults(base_dir, git_base_dir)
 
     @base_dir       = base_dir
     @repo           = Git.open(git_base_dir) if in_git_repo?
-    @version_helper = Bueller::VersionHelper.new(base_dir)
+    @version_helper = Bueller::VersionHelper.new gemspec
     @output         = $stdout
     @commit         = true
     @gemspec_helper = GemSpecHelper.new(gemspec, base_dir)
@@ -65,13 +63,13 @@ class Bueller
 
   # Writes out the gemspec
   def write_gemspec
-    Bueller::Commands::WriteGemspec.build_for(self).run
+    Bueller::Commands::WriteGemspec.run_for(self)
   end
 
   # Validates the project's gemspec from disk in an environment similar to how 
   # GitHub would build from it. See http://gist.github.com/16215
   def validate_gemspec
-    Bueller::Commands::ValidateGemspec.build_for(self).run
+    Bueller::Commands::ValidateGemspec.run_for(self)
   end
 
   # is the project's gemspec from disk valid?
@@ -81,56 +79,50 @@ class Bueller
 
   # Build a gem using the project's latest Gem::Specification
   def build_gem
-    Bueller::Commands::BuildGem.build_for(self).run
+    Bueller::Commands::BuildGem.run_for(self)
   end
 
   # Install a previously built gem
   def install_gem
-    Bueller::Commands::InstallGem.build_for(self).run
+    Bueller::Commands::InstallGem.run_for(self)
   end
 
   # Bumps the patch version.
   #
   # 1.5.1 -> 1.5.2
   def bump_patch_version()
-    Bueller::Commands::Version::BumpPatch.build_for(self).run
+    Bueller::Commands::Version::BumpPatch.run_for(self)
   end
 
   # Bumps the minor version.
   #
   # 1.5.1 -> 1.6.0
   def bump_minor_version()
-    Bueller::Commands::Version::BumpMinor.build_for(self).run
+    Bueller::Commands::Version::BumpMinor.run_for(self)
   end
 
   # Bumps the major version.
   #
   # 1.5.1 -> 2.0.0
   def bump_major_version()
-    Bueller::Commands::Version::BumpMajor.build_for(self).run
+    Bueller::Commands::Version::BumpMajor.run_for(self)
   end
 
   # Bumps the version, to the specific major/minor/patch version, writing out the appropriate version.rb, and then reloads it.
   def write_version(major, minor, patch, build, options = {})
-    command = Bueller::Commands::Version::Write.build_for(self)
-    command.major = major
-    command.minor = minor
-    command.patch = patch
-    command.build = build
-
-    command.run
+    command = Bueller::Commands::Version::Write.run_for(self, major, minor, patch, build)
   end
 
   def release_gem_to_github
-    Bueller::Commands::ReleaseToGithub.build_for(self).run
+    Bueller::Commands::ReleaseToGithub.run_for(self)
   end
 
   def release_to_git
-    Bueller::Commands::ReleaseToGit.build_for(self).run
+    Bueller::Commands::ReleaseToGit.run_for(self)
   end
 
   def release_gem_to_gemcutter
-    Bueller::Commands::ReleaseToGemcutter.build_for(self).run
+    Bueller::Commands::ReleaseToGemcutter.run_for(self)
   end
 
   def release_gem_to_rubyforge
@@ -141,13 +133,6 @@ class Bueller
     # no-op
   end
 
-  def check_dependencies(type = nil)
-    command = Bueller::Commands::CheckDependencies.build_for(self)
-    command.type = type
-
-    command.run
-  end
-
   def git_base_dir(base_dir = nil)
     if base_dir
       base_dir = File.dirname(base_dir)
@@ -156,11 +141,11 @@ class Bueller
     end
     return nil if base_dir==File.dirname("/")
     return base_dir if File.exists?(File.join(base_dir, '.git'))
-    return git_base_dir(base_dir)
+    git_base_dir(base_dir)
   end    
 
   def in_git_repo?
-    git_base_dir
+    !git_base_dir.nil?
   end
 
   def version_file_exists?
@@ -170,5 +155,4 @@ class Bueller
   def expects_version_file?
     gemspec.version.nil?
   end
-
 end
---
+++ spec/bueller_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+
+describe Bueller do
+  let(:gemspec) { Gemspec.build }
+  let(:bueller_with_git) { Bueller.new(gemspec, git_dir_path) }
+  let(:bueller_without_git) { Bueller.new(gemspec, non_git_dir_path) }
+  let(:bueller) { Bueller.new(gemspec, git_dir_path) }
+  let(:git_dir_path) { File.join(FileSystem.tmp_dir, 'git') }
+  let(:non_git_dir_path) { File.join(FileSystem.tmp_dir, 'nongit') }
+
+  describe '#in_git_repo?' do
+    it 'should return true if it is in a git repo' do
+      FileUtils.mkdir_p git_dir_path
+      Dir.chdir git_dir_path do
+        Git.init
+      end
+      bueller_with_git.should be_in_git_repo
+    end
+    it 'should return false if it is not in a git repo' do
+      FileUtils.mkdir_p non_git_dir_path
+      bueller_without_git.should_not be_in_git_repo
+    end
+  end
+
+  describe '#git_base_dir' do
+    it 'should find the base repo' do
+      bueller = Bueller.new(gemspec, File.dirname(File.expand_path(__FILE__)))
+      bueller.git_base_dir.should == File.dirname(File.dirname(File.expand_path(__FILE__)))
+    end
+  end
+
+  describe '#write_gemspec' do
+    it 'should build and run write gemspec command when writing gemspec' do
+      Bueller::Commands::WriteGemspec.should_receive(:run_for).with(bueller)
+
+      bueller.write_gemspec
+    end
+  end
+
+  describe '#validate_gemspec' do
+    it 'should build and run validate gemspec command when validating gemspec' do
+      Bueller::Commands::ValidateGemspec.should_receive(:run_for).with(bueller)
+
+      bueller.validate_gemspec
+    end
+  end
+
+  describe '#build_gem' do
+    it 'should build and run build gem command when building gem' do
+      Bueller::Commands::BuildGem.should_receive(:run_for).with(bueller)
+
+      bueller.build_gem
+    end
+  end
+
+  describe '#install_gem' do
+    it 'should build and run build gem command when installing gem' do
+      Bueller::Commands::InstallGem.should_receive(:run_for).with(bueller)
+
+      bueller.install_gem
+    end
+  end
+
+  describe '#bump_major_version' do
+    it 'should build and run bump major version command when bumping major version' do
+      Bueller::Commands::Version::BumpMajor.should_receive(:run_for).with(bueller)
+
+      bueller.bump_major_version
+    end
+  end
+
+  describe '#bump_major_version' do
+    it 'should build and run bump minor version command when bumping minor version' do
+      Bueller::Commands::Version::BumpMinor.should_receive(:run_for).with(bueller)
+
+      bueller.bump_minor_version
+    end
+  end
+
+  describe '#bump_major_version' do
+    it 'should build and run write version command when writing version' do
+      Bueller::Commands::Version::Write.should_receive(:run_for).with(bueller, 1, 5, 2, 'a1')
+
+      bueller.write_version(1, 5, 2, 'a1')
+    end
+  end
+
+  describe '#bump_major_version' do
+    it 'should build and run release to github command when running release_gem_to_github' do
+      Bueller::Commands::ReleaseToGithub.should_receive(:run_for).with(bueller)
+
+      bueller.release_gem_to_github
+    end
+  end
+
+  describe '#bump_major_version' do
+    it 'should build and run release to git command when running release_to_git' do
+      Bueller::Commands::ReleaseToGit.should_receive(:run_for).with(bueller)
+
+      bueller.release_to_git
+    end
+  end
+end
