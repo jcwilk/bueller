180111ecf9bf61abf283be345f42d1bb310cc6ba
Derek Kastner <dkastner@gmail.com>
2010-09-19 23:15:42 -0400

    Converted WriteGemspec

---
+++ spec/bueller/commands/write_gemspec_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Bueller::Commands::WriteGemspec do
+  context "after run" do
+    before :each do
+      @gemspec = Gem::Specification.new {|s| s.name = 'zomg' }
+      @gemspec_helper = Object.new
+      @gemspec_helper.stub!(:spec).and_return @gemspec
+      @gemspec_helper.stub!(:path).and_return 'zomg.gemspec'
+      @gemspec_helper.stub!(:write)
+
+      @output = StringIO.new
+
+      @version_helper = Object.new
+      @version_helper.stub!(:to_s).and_return '1.2.3'
+      @version_helper.stub!(:refresh)
+
+      @command = Bueller::Commands::WriteGemspec.new
+      @command.base_dir = 'tmp'
+      @command.version_helper = @version_helper
+      @command.gemspec = @gemspec
+      @command.output = @output
+      @command.gemspec_helper = @gemspec_helper
+
+      @now = Time.now
+      Time.stub!(:now).and_return @now
+    end
+
+    it "should refresh version" do
+      @version_helper.should_receive(:refresh)
+      @command.run
+    end
+
+    it "should update gemspec version" do
+      @command.run
+      @gemspec.version.to_s.should == '1.2.3'
+    end
+
+    it "should not refresh version neither update version if it's set on the gemspec" do
+      @gemspec.version = '2.3.4'
+      @command.run
+      @gemspec.version.to_s.should == '2.3.4'
+    end
+
+    it "should update gemspec date to the beginning of today" do
+      @command.run
+      @gemspec.date.should == Time.mktime(@now.year, @now.month, @now.day, 0, 0)
+    end
+
+    it "should write gemspec" do
+      @gemspec_helper.should_receive :write
+      @command.run
+    end
+
+    it "should output that the gemspec was written" do
+      @command.run
+      @output.string.should =~ /Generated: zomg\.gemspec/
+    end
+
+  end
+
+  BuildCommand.context self, "building for bueller" do
+    before :each do
+      @gemspec = Gem::Specification.new {|s| s.name = 'zomg' }
+      bueller = Bueller.new @gemspec
+      @command = Bueller::Commands::WriteGemspec.build_for(bueller)
+    end
+
+    it "should return WriteGemspec" do
+      @command.should be_a_kind_of Bueller::Commands::WriteGemspec
+    end
+  end
+end
--- spec/support/build_command.rb
+++ spec/support/build_command.rb
@@ -1,33 +1,29 @@
 module BuildCommand
-  def self.context(description, &block)
-    context description do
-      setup do
+  def self.context(env, description, &block)
+    env.context description do
+      before :each do
+        @repo           = mock(Object)
+        @commit         = mock(Object)
+        @version        = mock(Object)
+        @output         = mock(Object)
+        @base_dir       = mock(Object)
+        @gemspec_helper = mock(Object)
+        @rubyforge      = mock(Object)
 
-        @repo           = Object.new
-        @version_helper = Object.new
-        @gemspec        = Object.new
-        @commit         = Object.new
-        @version        = Object.new
-        @output         = Object.new
-        @base_dir       = Object.new
-        @gemspec_helper = Object.new
-        @rubyforge      = Object.new
+        @jeweler        = mock(Object)
 
-        @jeweler        = Object.new
-
-        stub(@jeweler).repo           { @repo }
-        stub(@jeweler).version_helper { @version_helper }
-        stub(@jeweler).gemspec        { @gemspec }
-        stub(@jeweler).commit         { @commit }
-        stub(@jeweler).version        { @version }
-        stub(@jeweler).output         { @output }
-        stub(@jeweler).gemspec_helper { @gemspec_helper }
-        stub(@jeweler).base_dir       { @base_dir }
-        stub(@jeweler).rubyforge    { @rubyforge }
+        @jeweler.stub!(:repo          ).and_return(@repo)
+        @jeweler.stub!(:version_helper).and_return(@version_helper)
+        @jeweler.stub!(:gemspec       ).and_return(@gemspec)
+        @jeweler.stub!(:commit        ).and_return(@commit)
+        @jeweler.stub!(:version       ).and_return(@version)
+        @jeweler.stub!(:output        ).and_return(@output)
+        @jeweler.stub!(:gemspec_helper).and_return(@gemspec_helper)
+        @jeweler.stub!(:base_dir      ).and_return(@base_dir)
+        @jeweler.stub!(:rubyforge     ).and_return(@rubyforge)
       end
 
-      context "", &block
+      instance_eval &block
     end
-
   end
 end
