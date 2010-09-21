991327eaf021badca6477382f72756f66450813f
Derek Kastner <dkastner@gmail.com>
2010-09-21 08:40:49 -0400

    Converting to RSpec

--- test/bueller/commands/test_validate_gemspec.rb
+++ test/bueller/commands/test_validate_gemspec.rb
@@ -1,27 +1,21 @@
-require 'test_helper'
+require 'spec_helper'
 
-class Bueller
-  module Commands
-    class TestValidateGemspec < Test::Unit::TestCase
+describe Bueller::Commands::ValidateGemspec do
+  BuildCommand.context "build context" do
+    before :each do
+      @command = Bueller::Commands::ValidateGemspec.build_for(@bueller)
+    end
 
-      BuildCommand.context "build context" do
-        setup do
-          @command = Bueller::Commands::ValidateGemspec.build_for(@bueller)
-        end
+    should "assign gemspec_helper" do
+      assert_same @gemspec_helper, @command.gemspec_helper
+    end
 
-        should "assign gemspec_helper" do
-          assert_same @gemspec_helper, @command.gemspec_helper
-        end
+    should "assign output" do
+      assert_same @output, @command.output
+    end
 
-        should "assign output" do
-          assert_same @output, @command.output
-        end
-
-        should "return Bueller::Commands::ValidateGemspec" do
-          assert_kind_of Bueller::Commands::ValidateGemspec, @command
-        end
-
-      end
+    should "return Bueller::Commands::ValidateGemspec" do
+      assert_kind_of Bueller::Commands::ValidateGemspec, @command
     end
   end
 end
