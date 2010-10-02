6c8d385354a2a075493d611a7d6478068b25400b
Derek Kastner <dkastner@gmail.com>
2010-10-02 15:02:45 -0400

    Finished ReleaseToGit

--- lib/bueller/commands/release_to_git.rb
+++ lib/bueller/commands/release_to_git.rb
@@ -1,15 +1,29 @@
 class Bueller
   module Commands
     class ReleaseToGit
-      attr_accessor :gemspec, :version, :repo, :output, :gemspec_helper, :base_dir
+      def self.run_for(bueller)
+        command = new bueller
+        command.run
+        command
+      end
 
-      def initialize(attributes = {})
+      attr_accessor :repo, :output
+      attr_reader :bueller
+
+      def initialize(bueller, attributes = {})
         self.output = $stdout
+        @bueller = bueller
 
         attributes.each_pair do |key, value|
           send("#{key}=", value)
         end
       end
+
+      def base_dir; bueller.base_dir; end
+      def gemspec; bueller.gemspec; end
+      def gemspec_helper; bueller.gemspec_helper; end
+      def version; bueller.version; end
+      def repo; bueller.repo; end
 
       def run
         raise "Hey buddy, try committing them files first" unless clean_staging_area?
@@ -17,40 +31,34 @@ class Bueller
         repo.checkout('master')
         repo.push
         
-        if release_not_tagged?
+        unless release_tagged?
           output.puts "Tagging #{release_tag}"
           repo.add_tag(release_tag)
 
           output.puts "Pushing #{release_tag} to origin"
           repo.push('origin', release_tag)
         end
-      end
+      end 
 
       def clean_staging_area?
-        status = repo.status
         status.added.empty? && status.deleted.empty? && status.changed.empty?
+      end
+
+      def status
+        repo.status
       end
 
       def release_tag
         "v#{version}"
       end
 
-      def release_not_tagged?
-        tag = repo.tag(release_tag) rescue nil
-        tag.nil?
-      end
-
-      def self.build_for(bueller)
-        command = self.new
-
-        command.base_dir = bueller.base_dir
-        command.gemspec = bueller.gemspec
-        command.version = bueller.version
-        command.repo = bueller.repo
-        command.output = bueller.output
-        command.gemspec_helper = bueller.gemspec_helper
-
-        command
+      def release_tagged?
+        begin
+          repo.tag(release_tag)
+          false
+        rescue 
+          true
+        end
       end
     end
   end
---
+++ spec/bueller/commands/release_to_git_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe Bueller::Commands::ReleaseToGit do
+  let(:bueller) { Bueller.new Gemspec.build }
+  let(:command) { Bueller::Commands::ReleaseToGit.new bueller, :output => StringIO.new }
+
+  before :each do
+    status = mock(Object)
+    bueller.repo = mock(Git, :status => status)
+
+    command.repo.status.stub!(:added).and_return []
+    command.repo.status.stub!(:changed).and_return []
+    command.repo.status.stub!(:deleted).and_return []
+  end
+
+  describe '#run' do
+    before :each do
+      command.repo.stub! :checkout
+      command.repo.stub! :push
+      command.stub!(:release_tagged?).and_return true
+    end
+
+    it 'should raise an error if the staging area is unclean' do
+      command.stub!(:clean_staging_area?).and_return false
+      expect { command.run }.should raise_error(RuntimeError, /try committing/i)
+    end
+    it 'should check out master' do
+      command.repo.should_receive(:checkout).with 'master'
+      command.run
+    end
+    it 'should push' do
+      command.repo.should_receive :push
+      command.run
+    end
+
+    it 'should not push tags if the release is already tagged' do
+      command.repo.should_not_receive :add_tag
+      command.repo.should_receive(:push).once
+
+      command.run
+    end
+    it 'should push tags if the release is not tagged' do
+      command.stub!(:release_tagged?).and_return false
+      command.repo.should_receive :add_tag
+      command.repo.should_receive(:push).twice
+
+      command.run
+    end
+  end
+
+  describe "#release_tagged?" do
+    it 'should be true if a tag exists' do
+      command.repo.stub!(:tag).and_raise RuntimeError
+      command.release_tagged?.should be_true
+    end
+    it 'should be false if no tag exists' do
+      command.repo.stub!(:tag)
+      command.release_tagged?.should be_false
+    end
+  end
+
+  describe '#clean_staging_area?' do
+    it 'should return true if there are no added, changed, or deleted files' do
+      command.should be_clean_staging_area
+    end
+    it 'should return false if there are added files' do
+      command.repo.status.stub!(:added).and_return ['Gemfile.lock']
+      command.should_not be_clean_staging_area
+    end
+    it 'should return false if there are changed files' do
+      command.repo.status.stub!(:changed).and_return ['Gemfile.lock']
+      command.should_not be_clean_staging_area
+    end
+    it 'should return false if there are deleted files' do
+      command.repo.status.stub!(:deleted).and_return ['Gemfile.lock']
+      command.should_not be_clean_staging_area
+    end
+  end
+end
