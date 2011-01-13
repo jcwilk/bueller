7dc4d89370e42883ec7f5051dbab6d8bf697f4ad
Derek Kastner <dkastner@gmail.com>
2011-01-13 11:19:25 -0500

    Release to gemcutter spec refactor

--- spec/bueller/commands/release_to_gemcutter_spec.rb
+++ spec/bueller/commands/release_to_gemcutter_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe Bueller::Commands::ReleaseToGemcutter do
-  let(:bueller) { Bueller.new Gemspec.build }
+  let(:gemspec_helper) { mock(Bueller::GemSpecHelper) }
+  let(:bueller) { mock(Bueller, :gemspec_helper => gemspec_helper) }
   let(:command) { Bueller::Commands::ReleaseToGemcutter.new bueller }
 
-  it "should push to gemcutter" do
-    command.gemspec_helper.stub!(:gem_path).and_return 'pkg/zomg-1.2.3.gem'
-    command.should_receive(:sh).with "bundle exec gem push pkg/zomg-1.2.3.gem"
-    command.run
+  describe '.run' do
+    it "should push to gemcutter" do
+      command.gemspec_helper.stub!(:gem_path).and_return 'pkg/zomg-1.2.3.gem'
+      command.should_receive(:sh).with "bundle exec gem push pkg/zomg-1.2.3.gem"
+      command.run
+    end
   end
 end
